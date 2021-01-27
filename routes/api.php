15edea09f213cae5ffad283bd6d948b251e2d832
YanaDev-2021 <78030341+YanaDev-2021@users.noreply.github.com>
2021-01-27 19:05:18 +0800

    CRUD Functionalities

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+Route::get('tasks', 'App\Http\Controllers\TaskController@index');
+Route::post('task', 'App\Http\Controllers\TaskController@store');
+Route::put('task/{id}', 'App\Http\Controllers\TaskController@update');
+Route::delete('task/{id}', 'App\Http\Controllers\TaskController@delete');
+
+
